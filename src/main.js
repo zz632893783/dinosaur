e15aa94213f230e860d1c38db359c5b2a65d9c3f
50525 <zheng.zhang@cpe-smartcity.com>
2023-05-05 17:44:20 +0800

    feat: update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { router } from '@/router/index.js'
+import { router } from './router/index.js'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './style/index.scss'
